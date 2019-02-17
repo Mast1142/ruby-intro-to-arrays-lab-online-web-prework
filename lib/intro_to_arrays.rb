58da470e4d801e512be6b2f142f6d861686da1f7
Samuel W Tong <ssamt1142@gmail.com>
2019-02-17 17:12:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  instantiate_new_array = []
+  return instantiate_new_array
+end
+
+def my_two_array
+  my_two_array = ["Lion", "Pride"]
+  return my_two_array
+end
+
+
+
